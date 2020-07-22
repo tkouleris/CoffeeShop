45fb699e6c2246e6801e05182ab4706ad92ff041
darkheart101 <kurokokoro@windowslive.com>
2020-07-22 21:42:39 +0300

    tree shaking

--- src/test/java/com/tkouleris/coffeeshop/Feature/TableControllerTest.java
+++ src/test/java/com/tkouleris/coffeeshop/Feature/TableControllerTest.java
@@ -5,22 +5,18 @@ import com.tkouleris.coffeeshop.model.Tables;
 import com.tkouleris.coffeeshop.repository.TablesRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @RunWith(SpringRunner.class)
-//@ExtendWith(SpringExtension.class)
 @WebMvcTest
 public class TableControllerTest {
 
