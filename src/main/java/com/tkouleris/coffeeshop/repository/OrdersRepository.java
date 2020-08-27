83edfbf9668c77e9d6e108aa98795c44e17cb3eb
darkheart101 <kurokokoro@windowslive.com>
2020-08-27 21:10:47 +0300

    feature: orders repository query for searching orders by table code

--- src/main/java/com/tkouleris/coffeeshop/repository/OrdersRepository.java
+++ src/main/java/com/tkouleris/coffeeshop/repository/OrdersRepository.java
@@ -1,9 +1,14 @@
 package com.tkouleris.coffeeshop.repository;
 
 import com.tkouleris.coffeeshop.model.Orders;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface OrdersRepository extends CrudRepository<Orders, Long> {
+    @Query("select o from Orders o left join Tables t where t.tableCode = ?1")
+    Optional<Orders> findByTableCode(String tableCode);
 }
