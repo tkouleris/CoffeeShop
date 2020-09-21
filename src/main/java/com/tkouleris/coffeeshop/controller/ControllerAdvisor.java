459022fadeb9d979c03b4ab879c3d0ef142ea830
darkheart101 <kurokokoro@windowslive.com>
2020-09-21 20:44:03 +0300

    chore: controller advisor

---
+++ src/main/java/com/tkouleris/coffeeshop/controller/ControllerAdvisor.java
@@ -0,0 +1,23 @@
+package com.tkouleris.coffeeshop.controller;
+
+import com.tkouleris.coffeeshop.dto.ApiResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ControllerAdvisor {
+    @Autowired
+    private ApiResponse apiResponse;
+
+    @ExceptionHandler({Exception.class})
+    public ResponseEntity<Object> handleGenericException(Exception e) {
+        String message = e.getMessage() != null ? e.getMessage():"Bad Request";
+        apiResponse.setMessage(message);
+        apiResponse.setData(null);
+
+        return new ResponseEntity<>(apiResponse.getBodyResponse(), HttpStatus.BAD_REQUEST);
+    }
+}
